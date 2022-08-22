a4e3f7cfeed09173a75920553dff149583aad940
yuminhwan <ymh7532@icloud.com>
2022-08-23 00:23:37 +0900

    test: 동시성 테스트

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -2,6 +2,10 @@ package com.example.stock.service;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -45,5 +49,29 @@ class StockServiceTest {
         // then
         Stock stock = stockRepository.findById(1L).get();
         assertThat(stock.getQuantity()).isEqualTo(99L);
+    }
+
+    @Test
+    void 동시에_100건의_요청() throws InterruptedException {
+        // given
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        // then
+        Stock stock = stockRepository.findById(1L).get();
+        assertThat(stock.getQuantity()).isZero();
     }
 }
