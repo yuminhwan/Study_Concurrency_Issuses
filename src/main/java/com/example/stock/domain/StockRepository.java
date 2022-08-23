746aec130fddc9e8494dd2dd9aa0b5383d4ed45a
yuminhwan <ymh7532@icloud.com>
2022-08-23 15:34:20 +0900

    feat: Pessimistic Lock 쿼리 구현

--- src/main/java/com/example/stock/domain/StockRepository.java
+++ src/main/java/com/example/stock/domain/StockRepository.java
@@ -1,6 +1,15 @@
 package com.example.stock.domain;
 
+import javax.persistence.LockModeType;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT s FROM Stock s WHERE s.id = :id")
+    Stock findByIdWithPessimisticLock(@Param("id") Long id);
 }
