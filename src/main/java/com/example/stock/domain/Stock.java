b879ecf85767e1e998f48513b0e4c74e054c4a00
yuminhwan <ymh7532@icloud.com>
2022-08-23 17:11:40 +0900

    refactor: version 필드 삭제

--- src/main/java/com/example/stock/domain/Stock.java
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Version;
 
 @Entity
 public class Stock {
@@ -16,10 +15,7 @@ public class Stock {
     private Long productId;
 
     private Long quantity;
-
-    @Version
-    private Long version;
-
+    
     protected Stock() {
     }
 
