00802918946a243e565a3d34cb741b9a81095c68
yuminhwan <ymh7532@icloud.com>
2022-08-24 20:55:26 +0900

    feat: redisson lock 기능 구현

---
+++ src/main/java/com/example/stock/service/redisson/RedissonLockStockFacade.java
@@ -0,0 +1,44 @@
+package com.example.stock.service.redisson;
+
+import java.util.concurrent.TimeUnit;
+
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.example.stock.service.StockService;
+
+@Component
+public class RedissonLockStockFacade {
+
+    private static final Logger log = LoggerFactory.getLogger(RedissonLockStockFacade.class);
+
+    private final RedissonClient redissonClient;
+    private final StockService stockService;
+
+    public RedissonLockStockFacade(RedissonClient redissonClient, StockService stockService) {
+        this.redissonClient = redissonClient;
+        this.stockService = stockService;
+    }
+
+    public void decrease(Long key, Long quantity) {
+        RLock lock = redissonClient.getLock(key.toString());
+
+        try {
+            boolean available = lock.tryLock(5, 1, TimeUnit.SECONDS);
+
+            if (!available) {
+                log.info("락 획득 실패");
+                return;
+            }
+
+            stockService.decrease(key, quantity);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+}
